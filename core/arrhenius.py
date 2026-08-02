87642aaddb452dfb8ff4ad960c5c27504f1d3d51
nessy <92063760+celedriel@users.noreply.github.com>
2026-08-02 12:44:01 -0300

    Update arrhenius.py

--- core/arrhenius.py
+++ core/arrhenius.py
@@ -1,1 +1,45 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import linregress
+import matplotlib.pyplot as plt
 
+class CineticaArrhenius:
+    def __init__(self, df_raw):
+        self.df = df_raw.copy()
+        self.results = {}
+        
+    def process_data(self):
+        self.df = self.df[(self.df['Temperatura (K)'] > 0) & (self.df['Tempo (s)'] > 0)].copy()
+        
+        self.df['k (1/s)'] = 1.0 / self.df['Tempo (s)']
+        self.df['1/T'] = 1.0 / self.df['Temperatura (K)']
+        self.df['ln(k)'] = np.log(self.df['k (1/s)'])
+        
+        return self.df
+        
+    def fit_model(self):
+        reg = linregress(self.df['1/T'], self.df['ln(k)'])
+        ea_j = -reg.slope * 8.314
+        
+        self.results = {
+            'Ea_J': ea_j,
+            'Ea_kJ': ea_j / 1000,
+            'ln_A': reg.intercept,
+            'R2': reg.rvalue**2,
+            'slope': reg.slope,
+            'intercept': reg.intercept
+        }
+        return self.results
+        
+    def plot_graph(self):
+        fig, ax = plt.subplots(figsize=(8, 5))
+        ax.plot(self.df['1/T'], self.df['ln(k)'], 'ro', label='Dados Experimentais')
+        ax.plot(self.df['1/T'], self.results['slope'] * self.df['1/T'] + self.results['intercept'], 
+                'k--', label=f"Arrhenius ($R^2={self.results['R2']:.4f}$)")
+        
+        ax.set_xlabel('1/T (1/K)')
+        ax.set_ylabel('ln(k)')
+        ax.set_title('Cinética de Arrhenius: ln(k) vs 1/T')
+        ax.legend()
+        ax.grid(True, linestyle='--', alpha=0.6)
+        return fig
